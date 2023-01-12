befdf4d18e6ab328cad5ae0120460546715963f0
이호종 <ccaja21@naver.com>
2023-01-12 15:06:53 +0900

    no message

---
+++ client/src/test/java/hj/lee/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package hj.lee.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
